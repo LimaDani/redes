9cf164aea38787b4b0fc1d80e0bcf0c134edc45d
Daniele Lima <danieleddelima@gmail.com>
2017-04-27 17:56:25 -0300

    Update server.c
    Code update

--- fisica/server.c
+++ fisica/server.c
@@ -1,47 +1,69 @@
-/* Referencia: http://www.programminglogic.com/example-of-client-server-program-in-c-using-sockets-and-tcp/ */
+/* Referencia antiga: http://www.programminglogic.com/example-of-client-server-program-in-c-using-sockets-and-tcp/ 
+   Referência atual:  http://www.binarytides.com/server-client-example-c-sockets-linux/
 
-#include <stdio.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <string.h>
+   Server:
+    1. Create socket
+	2. Bind to address and port
+	3. Put in listening mode
+	4. Accept connections and process there after.
+*/
 
-int main(){
-  int welcomeSocket, newSocket;
-  char buffer[1024];
-  struct sockaddr_in serverAddr;
-  struct sockaddr_storage serverStorage;
-  socklen_t addr_size;
-
-  /*---- Create the socket. The three arguments are: ----*/
-  /* 1) Internet domain 2) Stream socket 3) Default protocol (TCP in this case) */
-  welcomeSocket = socket(PF_INET, SOCK_STREAM, 0);
-  
-  /*---- Configure settings of the server address struct ----*/
-  /* Address family = Internet */
-  serverAddr.sin_family = AF_INET;
-  /* Set port number, using htons function to use proper byte order */
-  serverAddr.sin_port = htons(7891);
-  /* Set IP address to localhost */
-  serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-  /* Set all bits of the padding field to 0 */
-  memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);  
-
-  /*---- Bind the address struct to the socket ----*/
-  bind(welcomeSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
-
-  /*---- Listen on the socket, with 5 max connection requests queued ----*/
-  if(listen(welcomeSocket,5)==0)
-    printf("Listening\n");
-  else
-    printf("Error\n");
-
-  /*---- Accept call creates a new socket for the incoming connection ----*/
-  addr_size = sizeof serverStorage;
-  newSocket = accept(welcomeSocket, (struct sockaddr *) &serverStorage, &addr_size);
-
-  /*---- Send message to the socket of the incoming connection ----*/
-  strcpy(buffer,"Hello World\n");
-  send(newSocket,buffer,13,0);
-
-  return 0;
+#include<stdio.h>
+#include<string.h>     /*strlen*/
+#include<sys/socket.h>
+#include<arpa/inet.h> /*inet_addr*/
+#include<unistd.h>    /*write*/
+ 
+int main(int argc, char *argv[]){
+    int socket_desc, client_sock, c, read_size;
+    struct sockaddr_in server, client;
+    char client_message[2000];
+     
+    /*Create socket*/
+    socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_desc == -1)
+      printf("Could not create socket");
+    puts("Socket created");
+     
+    /*Prepare the sockaddr_in structure*/
+    server.sin_family      = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port        = htons(8888);
+     
+    /*Bind*/
+    if( bind(socket_desc,(struct sockaddr *)&server, sizeof(server)) < 0){
+        perror("bind failed. Error");
+        return 1;
+    }
+    puts("bind done");
+     
+    /*Listen*/
+    listen(socket_desc, 3);
+     
+    /*Accept and incoming connection*/
+    puts("Waiting for incoming connections...");
+    c = sizeof(struct sockaddr_in);
+     
+    /* Accept connection from an incoming client*/
+    client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c);
+    if (client_sock < 0){
+        perror("accept failed");
+        return 1;
+    }
+    puts("Connection accepted");
+     
+    /*Receive a message from client*/
+    while( (read_size = recv(client_sock, client_message, 2000, 0)) > 0 ){
+        /*Send the message back to client*/
+        write(client_sock ,"Connection made", strlen(client_message));
+    }
+     
+    if(read_size == 0){
+        puts("Client disconnected");
+        fflush(stdout);
+    }
+    else if(read_size == -1)
+        perror("recv failed");
+     
+    return 0;
 }
