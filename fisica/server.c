642e07f9849d2aff9aca490f35f9dd6c0a2935f1
Barbara Jaber <bpolianajaber@gmail.com>
2017-04-28 14:34:28 -0300

    server.c atualizando com comentarios

--- fisica/server.c
+++ fisica/server.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <strings.h>
 #include <sys/types.h> 
 #include <sys/socket.h>
-#include <netinet/in.h>
+#include <netinet/in.h> //estruturas para os endereços de domínio
 
 void error(char *msg)
 {
@@ -21,18 +19,20 @@ int main(int argc, char *argv[])
          fprintf(stderr,"ERROR, no port provided\n");
          exit(1);
      }
-     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+     sockfd = socket(AF_INET, SOCK_STREAM, 0); //Abre Socket para conexão 
      if (sockfd < 0) 
         error("ERROR opening socket");
      bzero((char *) &serv_addr, sizeof(serv_addr));
      portno = atoi(argv[1]);
-     serv_addr.sin_family = AF_INET;
+     // Configuração e conexão do Socket 
+     serv_addr.sin_family = AF_INET; // serv_addr é uma estrutura do tipo sin_addr
      serv_addr.sin_addr.s_addr = INADDR_ANY;
      serv_addr.sin_port = htons(portno);
      if (bind(sockfd, (struct sockaddr *) &serv_addr,
               sizeof(serv_addr)) < 0) 
               error("ERROR on binding");
-     listen(sockfd,5);
+     //
+     listen(sockfd,5); // Limite de backlog = 5, ou seja, no maximo 5 conexões 
      clilen = sizeof(cli_addr);
      newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
      if (newsockfd < 0) 
