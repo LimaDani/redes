262fe793f21a6eca5828786e3b979dbf02f63586
marco <marco@marco-IS-1413G.(none)>
2017-04-28 16:08:04 -0300

    Servidor em loop

--- fisica/server.c
+++ fisica/server.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
 #include <sys/types.h> 
 #include <sys/socket.h>
-#include <netinet/in.h> //estruturas para os endereços de domínio
+#include <netinet/in.h>
 
 void error(char *msg)
 {
@@ -11,37 +13,57 @@ void error(char *msg)
 
 int main(int argc, char *argv[])
 {
-     int sockfd, newsockfd, portno, clilen;
-     char buffer[256];
-     struct sockaddr_in serv_addr, cli_addr;
-     int n;
-     if (argc < 2) {
-         fprintf(stderr,"ERROR, no port provided\n");
-         exit(1);
-     }
-     sockfd = socket(AF_INET, SOCK_STREAM, 0); //Abre Socket para conexão 
-     if (sockfd < 0) 
-        error("ERROR opening socket");
-     bzero((char *) &serv_addr, sizeof(serv_addr));
-     portno = atoi(argv[1]);
-     // Configuração e conexão do Socket 
-     serv_addr.sin_family = AF_INET; // serv_addr é uma estrutura do tipo sin_addr
-     serv_addr.sin_addr.s_addr = INADDR_ANY;
-     serv_addr.sin_port = htons(portno);
-     if (bind(sockfd, (struct sockaddr *) &serv_addr,
-              sizeof(serv_addr)) < 0) 
-              error("ERROR on binding");
-     //
-     listen(sockfd,5); // Limite de backlog = 5, ou seja, no maximo 5 conexões 
-     clilen = sizeof(cli_addr);
-     newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-     if (newsockfd < 0) 
-          error("ERROR on accept");
-     bzero(buffer,256);
-     n = read(newsockfd,buffer,255);
-     if (n < 0) error("ERROR reading from socket");
-     printf("Here is the message: %s\n",buffer);
-     n = write(newsockfd,"I got your message",18);
-     if (n < 0) error("ERROR writing to socket");
-     return 0; 
+    int sockfd, sockfd2;
+    int port, tmq;
+    struct sockaddr_in server, client;
+    int client_l;   
+
+    char buffer[256];
+    int n;
+    
+    if (argc < 3){
+        fprintf(stderr,"[ INFO ] Usage: %s [PORT] [TMQ]\n", argv[0]);
+        exit(1);
+    }
+    
+    /* Criando socket */
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0){
+        printf("[ ERRO ] %d\n", __LINE__);
+    }
+
+    port = atoi(argv[1]);
+    tmq  = atoi(argv[2]);
+
+    server.sin_family      = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port        = htons(port);
+    
+    if(bind(sockfd, (struct sockaddr *) &server, sizeof(server)) < 0){
+        printf("[ ERRO ] %d\n", __LINE__);
+    }
+
+    listen(sockfd,5);
+    client_l = sizeof(client);
+    
+    while(1){
+        sockfd2 = accept(sockfd, (struct sockaddr *) &client, &client_l);
+        if(sockfd2 < 0){
+            printf("[ ERRO ] %d\n", __LINE__);
+        }
+    
+        bzero(buffer,256);
+        n = read(sockfd2, buffer, 255);
+        printf("%d", n);
+        if(n < 0){
+            printf("[ ERRO ] %d\n", __LINE__);
+        }
+    
+        printf("Here is the message: %s\n",buffer);
+        n = write(sockfd2,"I got your message",18);
+        if(n < 0){
+            printf("[ ERRO ] %d\n", __LINE__);
+        }
+    }
+    return 0; 
 }
