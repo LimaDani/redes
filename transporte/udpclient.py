9522b6fff9246322a80c16522399457ec98a180a
limadani <danieleddelima@gmail.com>
2017-07-01 05:48:32 -0300

    Update udpclient file

--- transporte/udpclient.py
+++ transporte/udpclient.py
@@ -2,21 +2,26 @@
 
 import socket
 import sys
+import os
 import subprocess
 
-# Criando um socket TCP/IP 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+f = open('clientlogudp.txt', 'w')
+#Criando um socket UDP 
+print >> sys.stderr, 'Criando um socket UDP'
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+f.writelines('Socket UDP criado\n')
 
-
-# Conectando o socket a porta onde o servidor esta escutando
+#conectando o socket a porta em que o servidor esta escutando 
 server_address = (sys.argv[1], 10000)
-print >>sys.stderr, 'Conectando a %s porta %s' % server_address
+print >>sys.stderr, '\nIniciando em %s porta %s' % server_address
+f.writelines('Conexao iniciada\n')
 
 try:
-    
-    # Chamando a Camada Fisica
-    subprocess.check_output(['./client', sys.argv[1], sys.argv[2]])
-    print >>sys.stderr, 'Envio de Transporte para Fisica'
+	#Chamando a Camada Fisica
+	subprocess.check_output(['./client', sys.argv[1], sys.argv[2]])
+	f.writelines('Envio da Camada de Transporte para Fisica efetuado\n')
+	print >>sys.stderr, 'Envio da Camada de Transporte para Fisica'
 
 finally:
-    print >>sys.stderr, 'Fechando socket Cliente UDP'
+	print >>sys.stderr, 'Socket fechado'
+	f.writelines('Conexao encerrada\n')
