95eab0697e7ec85ce1726a6481149b8afc8aeb0b
limadani <danieleddelima@gmail.com>
2017-07-01 05:49:06 -0300

    Update udpserver file

--- transporte/udpserver.py
+++ transporte/udpserver.py
@@ -1,14 +1,16 @@
-# Camada de Transporte - ServidorUDP 
+# Camada de Transporte - Servidor UDP 
 
 import socket
 import sys
 import subprocess
 
+f = open('serverlogudp.txt', 'w')
 try:
-    
-    # Chamando a Camada de Aplicacao
-    subprocess.check_output(["ruby" , "server.rb"])
-    print >>sys.stderr, 'Envio de Transporte para Aplicacao'
-
+	#Chamando a Camada de Aplicacao
+	subprocess.check_output(["ruby" , "server.rb"])
+	print >>sys.stderr, 'Envio de Transporte para Aplicacao'
+	f.writelines('Envio da camada de Transporte para Aplicacao efetuado\n')
 finally:
-    print >>sys.stderr, 'Fechando socket Servidor UDP'
+	f.writelines('Socket do Servidor UDP fechado\n')
+	print >>sys.stderr, 'Fechando socket Servidor UDP'
+	
